15a16d0690bfdfaf8787506032f968600ec4e81d
imhuwq <imhuwq@gmail.com>
2017-07-23 01:02:49 +0800

    new: python 3.5.3

---
+++ python3.5/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker build -t imhuwq/python3.5 .
---
+++ python3.5/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker rm -f python3.5
+sudo docker run -it --name python3.5 imhuwq/python3.5 /usr/bin/python
